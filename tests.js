f12ba7da22ec0a2d27acff0a619baecb9b3c4a5a
65Juko <thejulianjuko@gmail.com>
2022-08-24 13:17:07 +0200

    fix

--- tests.js
+++
@@ -1,6 +0,0 @@
-//jojojo
-let str =
-	Math.random().toString(36).substring(2, 15) +
-	Math.random().toString(36).substring(2, 15);
-base = new Buffer.from(str).toString("base64");
-console.log(base);
