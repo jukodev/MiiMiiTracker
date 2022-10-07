ae1ddbdb1d826e8eb65b3e9378aa694305172294
fabianmerkl <108922830+fabianmerkl@users.noreply.github.com>
2022-10-07 19:43:28 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ require("dotenv");
 
 async function createW2GRoom(uri) {
 	const { data } = await axios
-		.post("https://w2g.tv/rooms/create.json", {
+		.post("https://api.w2g.tv/rooms/create.json", {
 			w2g_api_key: process.env.W2G_API_KEY,
 			share: uri,
 			bg_color: "#2a354c",
